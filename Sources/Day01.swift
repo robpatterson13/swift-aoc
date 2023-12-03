ea0d3db8f0acacefd018c1c3dd7e213d349dd7dc
Rob <robertpatterson1313@gmail.com>
2023-12-03 17:38:34 -0500

    Update Day01.swift

--- Sources/Day01.swift
+++ Sources/Day01.swift
@@ -1,25 +1,34 @@
 import Algorithms
 
-struct Day00: AdventDay {
-  // Save your data in a corresponding text file in the `Data` directory.
-  var data: String
-
-  // Splits input data into its component parts and convert from string.
-  var entities: [[Int]] {
-    data.split(separator: "\n\n").map {
-      $0.split(separator: "\n").compactMap { Int($0) }
+struct Day01: AdventDay {
+    // Save your data in a corresponding text file in the `Data` directory.
+    var data: String
+    
+    // Splits input data into its component parts and convert from string.
+    var entities: [String] {
+        data.components(separatedBy: "\n")
     }
-  }
-
-  // Replace this with your solution for the first part of the day's challenge.
-  func part1() -> Any {
-    // Calculate the sum of the first set of input data
-    entities.first?.reduce(0, +) ?? 0
-  }
-
-  // Replace this with your solution for the second part of the day's challenge.
-  func part2() -> Any {
-    // Sum the maximum entries in each set of data
-    entities.map { $0.max() ?? 0 }.reduce(0, +)
-  }
+    
+    // Replace this with your solution for the first part of the day's challenge.
+    func part1() -> Any {
+        // Calculate the sum of the first set of input data
+        let stringOfNums = entities.map { s in
+            s.filter("0123456789".contains)
+        }.dropLast()
+        
+        let nums = stringOfNums.map {
+            String([$0.first!, $0.last!])
+        }.map {
+            Int($0) ?? 0
+        }
+        
+        return nums.reduce(0, +)
+    }
+    
+    // Replace this with your solution for the second part of the day's challenge.
+    func part2() -> Any {
+        // Sum the maximum entries in each set of data
+        return false
+        // entities.map { $0.max() ?? 0 }.reduce(0, +)
+    }
 }
